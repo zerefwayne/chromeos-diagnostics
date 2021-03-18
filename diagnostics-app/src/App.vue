d8ee783f827428646fb1b6586d6f25d4575b7b66
Aayush Joglekar <aayushjog@gmail.com>
2021-03-18 10:22:53 +0530

    [app:feat] Implement basic dashboard layout

--- diagnostics-app/src/App.vue
+++ diagnostics-app/src/App.vue
@@ -1,16 +1,47 @@
 <template>
   <div id="app">
-    <router-view/>
+    <div id="app-container">
+      <div id="app-sidebar">Sidebar</div>
+      <div id="app-router">
+        <router-view />
+      </div>
+    </div>
   </div>
 </template>
 
 <style lang="scss">
 #app {
   font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
+  color: white;
+  background-color: #272b2f;
+  height: 100vh;
+  width: 100%;
+  padding: 4em;
+}
+
+#app-container {
+  height: 100%;
+  width: 100%;
+  background-color: #212529;
+  border-radius: 1.5em;
+  display: flex;
+  overflow: hidden;
+}
+
+#app-sidebar {
+  flex: 0 0 20%;
+  background-color: #2A2E32;
+  padding: 1em 2em;
+  display: flex;
+  flex-direction: column;
+}
+
+#app-router {
+  flex: 1;
+  background-color: #212529;
+  display: flex;
+  flex-direction: column;
+  padding: 1em 2em;
 }
 
 #nav {
