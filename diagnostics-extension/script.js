91696dc3b4d8e80db0649e3524b076bc2191024d
Aayush Joglekar <aayushjog@gmail.com>
2021-03-18 13:54:29 +0530

    [ext:feat] Fetch cpu, memory, storage usage

--- diagnostics-extension/script.js
+++ diagnostics-extension/script.js
@@ -1,3 +1,25 @@
-setInterval(() => {
-  console.log("Running!");
-}, 1000);
+const REFRESH_TIME = 1000;
+
+const fetchProcessorUsage = () => {
+  chrome.system.cpu.getInfo((data) => {
+    console.log(data);
+  });
+};
+
+const fetchMemoryUsage = () => {
+  chrome.system.memory.getInfo((data) => {
+    console.log(data);
+  });
+};
+
+const fetchStorageUsage = () => {
+  chrome.system.storage.getInfo((data) => {
+    console.log(data);
+  });
+};
+
+setInterval(async () => {
+  fetchProcessorUsage();
+  fetchMemoryUsage();
+  fetchStorageUsage();
+}, REFRESH_TIME);
