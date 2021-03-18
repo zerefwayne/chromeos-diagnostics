4f6938e243629d9c35e7875dcd137e74dc7357d6
Aayush Joglekar <aayushjog@gmail.com>
2021-03-18 11:09:32 +0530

    [app:feat] Implement router

--- diagnostics-app/src/App.vue
+++ diagnostics-app/src/App.vue
@@ -3,7 +3,26 @@
     <div id="app-container">
       <div id="app-sidebar">
         <div id="app-branding">
-          <img src="./assets/logo.png" alt="ChromeOS Diagnostics" height="35%" />
+          <img
+            src="./assets/logo.png"
+            alt="ChromeOS Diagnostics"
+            height="37%"
+          />
+        </div>
+        <div id="app-nav">
+          <div class="nav-heading">NAVIGATION</div>
+          <ul>
+            <router-link
+              tag="li"
+              v-for="item in navigationItems"
+              :to="item.to"
+              v-bind:key="item.alt"
+              active-class="active-li"
+            >
+              <img :src="item.icon" :alt="item.alt" />
+              <p>{{ item.text }}</p>
+            </router-link>
+          </ul>
         </div>
       </div>
       <div id="app-router">
@@ -13,9 +32,49 @@
   </div>
 </template>
 
+<script lang="ts">
+import Vue from "vue";
+
+import DataIcon from "@/assets/icons/data-light.svg";
+import DiagIcon from "@/assets/icons/diag-light.svg";
+import StateIcon from "@/assets/icons/state-light.svg";
+
+export default Vue.extend({
+  data() {
+    return {
+      navigationItems: [
+        {
+          alt: "Data",
+          icon: DataIcon,
+          text: "System Data",
+          to: "data",
+        },
+        {
+          alt: "State",
+          icon: StateIcon,
+          text: "System State",
+          to: "state",
+        },
+        {
+          alt: "Diagnostics",
+          icon: DiagIcon,
+          text: "Diagnostics Manager",
+          to: "diagnostics",
+        },
+      ],
+    };
+  },
+  methods: {
+    getImage(url: string) {
+      return require(url);
+    },
+  },
+});
+</script>
+
 <style lang="scss">
 #app {
-  font-family: 'Roboto', Avenir, Helvetica, Arial, sans-serif;
+  font-family: "Roboto", Avenir, Helvetica, Arial, sans-serif;
   color: white;
   background-color: #272b2f;
   height: 100vh;
@@ -34,7 +93,7 @@
 
 #app-sidebar {
   flex: 0 0 20%;
-  background-color: #2A2E32;
+  background-color: #2a2e32;
   padding: 1em 2em;
   display: flex;
   flex-direction: column;
@@ -53,6 +112,49 @@
   display: flex;
   justify-content: center;
   align-items: center;
+}
+
+#app-nav {
+  padding: 1em;
+
+  .nav-heading {
+    font-family: "Nunito";
+    font-weight: bold;
+    color: #a9a9a9;
+  }
+
+  ul {
+    list-style: none;
+    margin-top: 1em;
+  }
+
+  li {
+    margin-top: 0.5em;
+    font-size: 1.1em;
+    display: flex;
+    align-items: center;
+    cursor: pointer;
+    color: #c7c7c7;
+    padding: 0.5em 1em;
+    border-radius: 5px;
+
+    p {
+      text-align: center;
+    }
+
+    img {
+      height: 1.2em;
+      margin-right: 1em;
+      fill: pink;
+    }
+  }
+
+  .active-li {
+    background-color: #3a6cd7;
+    color: white;
+    padding-left: 1.1em;
+    transition: all 200ms ease-out;
+  }
 }
 
 #nav {
