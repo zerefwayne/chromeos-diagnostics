3cc9a86620dedbab962bc0f4d7f09ec488847c56
Aayush Joglekar <aayushjog@gmail.com>
2021-03-18 10:10:54 +0530

    [app:feat] Add dashboard pages

---
+++ diagnostics-app/src/views/DiagnosticsManager.vue
@@ -0,0 +1,18 @@
+<template>
+  <div>
+    <h2>
+      Diagnostics Manager
+    </h2>
+  </div>
+</template>
+
+<script lang="ts">
+import Vue from 'vue'
+export default Vue.extend({
+  
+})
+</script>
+
+<style lang="scss" scoped>
+
+</style>
---
+++ diagnostics-app/src/views/State.vue
@@ -0,0 +1,18 @@
+<template>
+  <div>
+    <h2>
+      State
+    </h2>
+  </div>
+</template>
+
+<script lang="ts">
+import Vue from 'vue'
+export default Vue.extend({
+  
+})
+</script>
+
+<style lang="scss" scoped>
+
+</style>
